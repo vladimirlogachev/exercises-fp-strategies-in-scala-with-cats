0df92af061311e8ca66c2ccd240cb4b4c7e4b338
Vladimir Logachev <vladimir@logachev.dev>
2024-07-20 20:30:02 +0400

    2.2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,3 +3,20 @@
   println(msg)
 
 def msg = "I was compiled by Scala 3. :)"
+
+/*
+
+Ch. 2.2
+
+Exercise: Tree
+To gain a bit of practice defining algebraic data types, code the following description in Scala
+(your choice of version, or do both.).
+A Tree with elements of type A is:
+• a Leaf with a value of typeA; or
+• a Node with a left and right child, which are both Trees with elements of type A.
+ */
+
+enum Tree[A] {
+  case Leaf(a: A)
+  case Node(left: A, right: A)
+}
