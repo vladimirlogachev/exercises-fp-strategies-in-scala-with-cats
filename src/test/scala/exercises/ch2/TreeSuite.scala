00dcdcc05f665893a7c7c2b191dbba31cd6b1c7a
Vladimir Logachev <vladimir@logachev.dev>
2024-07-31 19:08:25 +0400

    Simplify tests

--- src/test/scala/exercises/ch2/TreeSuite.scala
+++ src/test/scala/exercises/ch2/TreeSuite.scala
@@ -3,7 +3,7 @@ package exercises.ch2
 import cats.implicits._
 import weaver._
 
-object TreeSuite extends SimpleIOSuite {
+object TreeSuite extends FunSuite {
   import Tree._
 
   def buildTreeLeft[A](size: BigInt, fillFromIndex: BigInt => A): Tree[A] =
@@ -17,61 +17,61 @@ object TreeSuite extends SimpleIOSuite {
     }
     buildTreeLeftRecursive(size - 1, Leaf(fillFromIndex(size - 1)))
 
-  pureTest("size (simple)") {
+  test("size (simple)") {
     val tree = Node(Leaf(1), Node(Leaf(2), Leaf(4)))
 
     expect.eql(tree.size, 3: BigInt)
   }
 
-  pureTest("tree built of size 3") {
+  test("tree built of size 3") {
     val tree                       = buildTreeLeft(3, identity)
     val expectedTree: Tree[BigInt] = Node(Node(Leaf(2), Leaf(1)), Leaf(0))
 
     expect.eql(tree.size, 3: BigInt) and expect.eql(tree, expectedTree)
   }
 
-  pureTest("size is stack-safe (supports large trees)") {
+  test("size is stack-safe (supports large trees)") {
     val size = BigInt(1_000_000)
     val tree = buildTreeLeft(size, identity)
 
     expect.eql(tree.size, size)
   }
 
-  pureTest("contains (simple)") {
+  test("contains (simple)") {
     val tree = Node(Leaf(1), Node(Leaf(2), Leaf(4)))
 
     expect(tree.contains(1)) and expect(tree.contains(2)) and expect(!tree.contains(3))
   }
 
-  pureTest("contains is stack-safe (supports large trees)") {
+  test("contains is stack-safe (supports large trees)") {
     val size = BigInt(1_000_000)
     val tree = buildTreeLeft(size, identity)
 
     expect(tree.contains(0)) and expect(tree.contains(size - 1)) and expect(!tree.contains(size))
   }
 
-  pureTest("map simple case 1") {
+  test("map simple case 1") {
     val intTree    = Node(Leaf(1), Node(Leaf(2), Leaf(4)))
     val stringTree = Node(Leaf("1"), Node(Leaf("2"), Leaf("4")))
 
     expect.eql(intTree.map(_.toString), stringTree)
   }
 
-  pureTest("map simple case 2") {
+  test("map simple case 2") {
     val intTree    = Node(Node(Leaf(1), Leaf(2)), Leaf(4))
     val stringTree = Node(Node(Leaf("1"), Leaf("2")), Leaf("4"))
 
     expect.eql(intTree.map(_.toString), stringTree)
   }
 
-  pureTest("map: identity, small tree") {
+  test("map: identity, small tree") {
     val size = BigInt(10)
     val tree = buildTreeLeft(size, identity)
 
     expect.eql(tree.map(identity), tree)
   }
 
-  pureTest("map: identity, large tree") {
+  test("map: identity, large tree") {
     val size       = BigInt(1_000_000)
     val tree       = buildTreeLeft(size, identity)
     val mappedTree = tree.map(identity)
