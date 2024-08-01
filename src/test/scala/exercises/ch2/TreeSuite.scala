8db965872fed1333f65214631f6cb4756a324abd
Vladimir Logachev <vladimir@logachev.dev>
2024-08-01 18:42:35 +0400

    Rearrange test helpers

--- src/test/scala/exercises/ch2/TreeSuite.scala
+++ src/test/scala/exercises/ch2/TreeSuite.scala
@@ -2,25 +2,29 @@ package exercises.ch2
 
 import cats.implicits._
 import weaver._
-
 import Tree._
 
-def buildTreeLeft[A](size: BigInt, fillFromIndex: BigInt => A): Tree[A] =
-  @scala.annotation.tailrec
-  def buildTreeLeftRecursive(remainingSize: BigInt, accumulator: Tree[A]): Tree[A] = {
-    if (remainingSize === 0) accumulator
-    else {
-      val newTree = Node(accumulator, Leaf(fillFromIndex(remainingSize - 1)))
-      buildTreeLeftRecursive(remainingSize - 1, newTree)
+object Helpers {
+  def buildTreeLeft[A](size: BigInt, fillFromIndex: BigInt => A): Tree[A] =
+    @scala.annotation.tailrec
+    def buildTreeLeftRecursive(remainingSize: BigInt, accumulator: Tree[A]): Tree[A] = {
+      if (remainingSize === 0) accumulator
+      else {
+        val newTree = Node(accumulator, Leaf(fillFromIndex(remainingSize - 1)))
+        buildTreeLeftRecursive(remainingSize - 1, newTree)
+      }
     }
-  }
-  buildTreeLeftRecursive(size - 1, Leaf(fillFromIndex(size - 1)))
+    buildTreeLeftRecursive(size - 1, Leaf(fillFromIndex(size - 1)))
+
+  val largeSize = BigInt(1_000_000)
+  val largeTree = buildTreeLeft(largeSize, identity)
+}
 
 object BuildTreeSuite extends SimpleIOSuite {
 
-  loggedTest("buildTreeLeft example") { log =>
+  loggedTest("Helpers.buildTreeLeft example") { log =>
     val size                       = BigInt(3)
-    val tree: Tree[BigInt]         = buildTreeLeft(size, identity)
+    val tree: Tree[BigInt]         = Helpers.buildTreeLeft(size, identity)
     val expectedTree: Tree[BigInt] = Node(Node(Leaf(2), Leaf(1)), Leaf(0))
     for {
       _ <- log.info(tree.toString)
@@ -32,9 +36,6 @@ object BuildTreeSuite extends SimpleIOSuite {
 
 object TreeSuite extends FunSuite {
 
-  val largeSize = BigInt(1_000_000)
-  val largeTree = buildTreeLeft(largeSize, identity)
-
   test("size (simple)") {
     val tree = Node(Leaf(1), Node(Leaf(2), Leaf(4)))
 
@@ -42,14 +43,14 @@ object TreeSuite extends FunSuite {
   }
 
   test("tree built of size 3") {
-    val tree                       = buildTreeLeft(3, identity)
+    val tree                       = Helpers.buildTreeLeft(3, identity)
     val expectedTree: Tree[BigInt] = Node(Node(Leaf(2), Leaf(1)), Leaf(0))
 
     expect.eql(tree.size, 3: BigInt) and expect.eql(tree, expectedTree)
   }
 
   test("size is stack-safe (supports large trees)") {
-    expect.eql(largeTree.size, largeSize)
+    expect.eql(Helpers.largeTree.size, Helpers.largeSize)
   }
 
   test("contains (simple)") {
@@ -59,11 +60,11 @@ object TreeSuite extends FunSuite {
   }
 
   test("contains is stack-safe (supports large trees) (expected slow)") {
-    expect(largeTree.contains(0))
+    expect(Helpers.largeTree.contains(0))
   }
 
   test("contains is stack-safe (supports large trees) (expected quick)") {
-    expect(largeTree.contains(largeSize - 1))
+    expect(Helpers.largeTree.contains(Helpers.largeSize - 1))
   }
 
   test("map simple case 1") {
@@ -82,13 +83,13 @@ object TreeSuite extends FunSuite {
 
   test("map: identity, small tree") {
     val size = BigInt(10)
-    val tree = buildTreeLeft(size, identity)
+    val tree = Helpers.buildTreeLeft(size, identity)
 
     expect.eql(tree.map(identity), tree)
   }
 
   test("map: identity, large tree") {
-    expect.eql(largeTree.map(identity), largeTree)
+    expect.eql(Helpers.largeTree.map(identity), Helpers.largeTree)
   }
 
 }
